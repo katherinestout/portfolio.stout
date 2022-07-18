ba2f2c63fae6e6dcf62de4369d021a27a5b35c4d
Katie Stout <katiestout@katies-mbp.myfiosgateway.com>
2022-07-18 11:41:59 -0400

    header graphic

--- portfolio/src/components/header/Header.js
+++ portfolio/src/components/header/Header.js
@@ -1,10 +1,13 @@
 import styles from "./header.scss";
+import headerGraphic from "./../../svg/headerGraphic.png";
 
 const Header = () => (
   <div className={styles}>
+    <img src={headerGraphic} alt="headerGraphic" className="headerGraphic"/>
     <header>
-      <h1>Katie Stout</h1>
-      <h2>UI/UX Designer </h2>
+      {/* <h1>Katie Stout</h1>
+      <h2>UI/UX Designer </h2> */}
+      
     </header>
   </div>
 );
