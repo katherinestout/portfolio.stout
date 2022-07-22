8e483e5d5daf421086dc21c621fe270248553a0d
Katie Stout <katiestout@Katies-MacBook-Pro.local>
2022-07-22 19:33:14 -0400

    sitemap

--- portfolio/src/routes/projects/components/crewCuts.js
+++ portfolio/src/routes/projects/components/crewCuts.js
@@ -1,16 +1,26 @@
 import React from "react";
 import styles from "./../styles/crewCuts.scss";
 import ccGraphic from "./../../../svg/crewCuts/crewcutssq.png";
+import talkBubbles from "./../../../svg/crewCuts/talkBubbles.png";
+import sitemap from "./../../../svg/crewCuts/sitemapCrewCuts.png"
 
 const CrewCuts = () => (
   <div className={styles}>
     <div className="crewCutsBody">
+        <div className="crewCutsContent">
 
       <img src={ccGraphic} alt="ccGraphic" className="ccGraphic" />
+      <ul>
+          <li>Empathize</li>
+          <li>Define</li>
+          <li>Ideate</li>
+          <li>Prototype</li>
+          <li>Test</li>
+      </ul>
       <div className="wrapper-1">
         <div className="div1">
        <div className = "header"> Responsibilities</div> 
-          <ul>
+          <ul className="content">
               <li>User Research</li>
               <li>Wireframes</li>
               <li>Persona</li>
@@ -21,11 +31,12 @@ const CrewCuts = () => (
         </div>
         <div className="div2">
           <div className="header">Background </div>
-          
-          Crew Cuts is a small business ran by two brothersin Silver
+          <div className = "content">
+          Crew Cuts is a small business ran by two brothers in Silver
           Spring, MD, offering various services such as lawn care, moving, and
           gardening. They are in the process of building their website, but
           currently do not have another online presence.
+          </div>
         </div>
       </div>
       <div className="wrapper-2">
@@ -87,24 +98,28 @@ const CrewCuts = () => (
        Would
       rather hire a Small local business rather than a big company </p>
 
-      <p>
-      50% hire
+      <p className="content center">
+ 50% hire
       small business or do it themselves
-      </p>
-      <p>
+  
       50% hire big well known companies
       </p>
-      <p>
-      Of
-      the 50% that hire big companies, 100% of users said to sway them to hire a
-      small business they would upon consideration based on reviews online, or
-      recommendations from someone they already know
+      <p className="content center">
+      Of the 50% that hire big companies, 
+   
+      100% of users said to sway them to hire 
+ 
+      a small business they would upon consideration 
+
+      based on reviews online, or recommendations 
+
+      from someone they already know
       </p>
         </div>
 
       </div>
  
-      <div className="wrapper-4">
+      <div className="wrapper-4 center">
         <div className="header div1">Key themes found in survey:</div>
         <div className="subHeader div2">Timeliness</div>
         <div className="subHeader div3">Trustworthiness</div>
@@ -112,28 +127,69 @@ const CrewCuts = () => (
       </div>
     
   
-      
-      
-      Persona Jan Gardiner The
-      Persona JAN Jan Gardiner, 30 Teacher Married Silver Spring, MD When it
-      comes to taking care of your yard and outdoor spaces, what prevents you
-      from booking a local lawn care service versus a big company service?
-      Definitely when there is no proven work to show, and no way of knowing how
-      trustworthy someone is. I am busy all the time so I don’t have time to
-      keep watch when someone is hired to work. Needs: Easily see before and
+      <div className="wrapper-5">
+      <div className="div1 header">
+      Persona
+      </div>
+      <div className="div2 subHeader">
+      Jan Gardiner
+      </div>
+      <div className="div3">
+      <ul className="content">
+          <li className="personaName">JAN</li>
+          <li>Jan Gardiner, 30</li>
+          <li>Teacher</li>
+          <li>Married</li>
+          <li>Silver Spring, MD</li>
+      </ul>
+      </div>
+      <div className="div4">
+          
+      </div>
+      </div>
+        
+        <div className= "center">
+        <img src = {talkBubbles} alt="talkbubbles" className="talkBubbles" />
+        </div>
+    
+     <div className="wrapper-6">
+     <div className="div1 header">
+     Needs 
+     </div>
+
+     <div className="div2 content">
+     Easily see before and
       after photos of gardening. Reviews to show trustworthiness and quality of
-      work. Frustrations: I can’t find a lawn-service company that is timely,
-      trustworthy, or has a proven track record. As a homeowner, I want someone
-      to plant my mums this Spring, because I don’t have time to garden. Jan
-      Gardiner is a 30 year old homeowner who needs a lawn-care service to plant
-      her mums because she doesn’t have time. Jan Gardiner is a 30 year old
-      homeowner who needs a trusted lawn-care service to plant her mums while
-      she is at work because she doesn’t have time to watch over who she hires.
+      work. 
+    
+     </div>
+     <div className="div3 header">Frustrations</div>
+     <div className="div4 content"> I can’t find a lawn-service company that is timely, trustworthy, or has a proven track record. </div>
+     
+     <div className="div5 header">Problem Statement</div>
+     <div className="div6 content">
       Jan Gardiner is a 30 year old homeowner who needs a lawn-care service that
-      is experienced and has proven results to plant her garden this spring
-      because last year she hired a company to plant her garden and they killed
-      all her flowers. Sitemap Wireframes Low-Fidelity Prototype User Testing
-      High-Fidelity Prototype
+      is experienced and has proven results, to plant her garden this Spring
+      because she does not have time with her schedule. 
+      She also does not have time to supervise who she hires, which makes her feel concerned.</div>
+     </div>
+      
+      
+        
+
+      
+      <div className = "wrapper-7">
+          <div className="header div1"> Sitemap </div>
+          <div div2> <img src={sitemap} alt="sitemapcc" className="ccsitemap" /></div>
+          <div className="header">Wireframes</div>
+          <div></div>
+          <div className="header">Low-Fidelity Prototype</div>
+          <div></div>
+          <div className="header">High-Fidelity Prototype</div>
+          <div></div>
+
+      </div>
+    </div>
     </div>
   </div>
 );
