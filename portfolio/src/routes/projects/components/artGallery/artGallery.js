d17f04da532b05413163fdf63bdefedc23d72547
Katie Stout <katie.stout@outlook.com>
2022-08-23 20:15:34 -0400

    art gallery fix

--- portfolio/src/routes/projects/components/artGallery/artGallery.js
+++ portfolio/src/routes/projects/components/artGallery/artGallery.js
@@ -1,147 +1,86 @@
 import React from "react";
-import styles from './../styles/galleryApp.scss';
+// import styles from './../../styles/galleryApp.scss';
 
-const ArtGallery = () => 
-
-
-<div className = {styles}>
+const ArtGallery = () => (
+  <div>
     <div>
-    Art Gallery App
-
-    Responsibilities
-    <ul className="content">
-              <li>User Research</li>
-              <li>Wireframes</li>
-              <li>Persona</li>
-              <li>Usability Analyst</li>
-              <li>Low-Fidelity Prototype</li>
-              <li>High-Fidelity Prototype</li>
-            </ul>
-
-
-
-    Background
-        The Product
-
-        The Artist Bio App for an Art Gallery in Edinburgh 
-        was designed to connect our target users 
-        ( users who are interested in obtaining art 
-        pieces such as art collectors ) with other artists via the app.
-
-        This App was one of the projects completed from beginning to end 
-        while I obtained the Certificate in UX design from Coursera.
-        We utilized the five stages of the design thinking process:
-
-        Empathize
-        Define
-        Ideate
-        Prototype 
-        Test
-
-        Product Duration
-        October 2021 - December 2021
-
-        Problem
-            
-
-        Solution
-
-            The goal was to create an app that allowed for users to 
-            easily find art they wanted to obtain and to be able 
-            to contact the artist, to spark the conversation that 
-            could eventually lead to more opportunities for the 
-            artist such as the user purchasing their art or their 
-            art being showed in other galleries as well.
-
-
-
-
-        Understanding the User - Empathize / Define
-            User Research 
-                Summary 
-                    I found that users wanted a variety of 
-                    art to select from. Users also wanted 
-                    to be able to contact artists directly 
-                    and from the specific gallery websites. 
-                    Often galleries don’t list contact information 
-                    and make users directly contact the gallery instead. 
-                    Users also wanted to be able to see art and maybe 
-                    not necessarily purchase it, and because of the 
-                    pandemic galleries should adapt to an online audience.
-
-                User Pain Points
-                    1. Galleries don't have a way for me to contact artists
-                        Provide multiple ways to contact artists, 
-                        such as a contact form, the artist’s 
-                        contact information, and the artist’s 
-                        individual website.
-                    2. Because of the pandemic, I cannot get to galleries in person.
-                       Provide high quality images.
-                    3. Galleries often have limited selection of art, I cannot find what I am looking for.
-                        Provide multiple ways of searching for the art via the gallery app. Can put genre, artist, and multiple genres. Provide a search bar.
-
-                
-            Personas
-                Tiffany Young
-                Problem statement:
-                    Tiffany Young is an art dealer who needs to be able to view art virtually because she doesn’t have time to travel.
-                IMAGE OF PERSONA / with stuff on it
-            
-            
-            User Journey Map
-                Mapping Tiffany’s journey shows several problems 
-                that could arise with the app. Definitely simplicity/ 
-                conciseness in the menu style and high quality photos are a must.
-
-                Persona: Tiffany Young
-                Goal: Find new art pieces for my gallery.
-
-                IMAGE OF USER JOURNEY MAP
-
-        Starting The Design - Ideate / Prototype / Test
-        
-        Paper Wireframes
-            Goals for paper wireframes: The paper wireframes should have plenty of space for the images since it is an image centered app.
-        Digital Wireframes
-            SEE SLIDESHOW IMAGE INSERT GRAPHIC WITH LABELS
-        Low-Fidelity Prototypes
-            Link to low fi prototypes
-            https://www.figma.com/proto/BkvbbGLoYEyj2PUKLNQsWp/EdinburghGalleryApp?node-id=1%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=8%3A415
-        Usability Studies
-            I conducted two rounds of usability studies. 
-            Findings from the first study helped guide the designs 
-            from wireframes to mockups. The second study used a high 
-            fidelity prototype and revealed what aspects of the mockups 
-            needed refining.
-
-            Round One Findings:
-            1. Users want to easily find what art They are looking for.
-            2. Users want to be able to view art in Great detail.
-
-            Round Two Findings:
-            1. Users want to be able to be directed somewhere after filling out contact form.
-            2. Users want to get a confirmation email.
-
-        Refining the Design: 
-
-        High-Fidelity Prototype
+      Art Gallery App
+      <div>
+        Responsibilities
+        <ul className="content">
+          <li>User Research</li>
+          <li>Wireframes</li>
+          <li>Persona</li>
+          <li>Usability Analyst</li>
+          <li>Low-Fidelity Prototype</li>
+          <li>High-Fidelity Prototype</li>
+        </ul>
+      </div>
+      <div>
+        Background The Product The Artist Bio App for an Art Gallery in
+        Edinburgh was designed to connect our target users ( users who are
+        interested in obtaining art pieces such as art collectors ) with other
+        artists via the app. This App was one of the projects completed from
+        beginning to end while I obtained the Certificate in UX design from
+        Coursera. We utilized the five stages of the design thinking process:
+        Empathize Define Ideate Prototype Test Product Duration October 2021 -
+        December 2021
+      </div>
+      <div>Problem</div>
+      <div>
+        Solution The goal was to create an app that allowed for users to easily
+        find art they wanted to obtain and to be able to contact the artist, to
+        spark the conversation that could eventually lead to more opportunities
+        for the artist such as the user purchasing their art or their art being
+        showed in other galleries as well.
+      </div>
+      <div>
+        Understanding the User - Empathize / Define User Research Summary I
+        found that users wanted a variety of art to select from. Users also
+        wanted to be able to contact artists directly and from the specific
+        gallery websites. Often galleries don’t list contact information and
+        make users directly contact the gallery instead. Users also wanted to be
+        able to see art and maybe not necessarily purchase it, and because of
+        the pandemic galleries should adapt to an online audience. User Pain
+        Points 1. Galleries don't have a way for me to contact artists Provide
+        multiple ways to contact artists, such as a contact form, the artist’s
+        contact information, and the artist’s individual website. 2. Because of
+        the pandemic, I cannot get to galleries in person. Provide high quality
+        images. 3. Galleries often have limited selection of art, I cannot find
+        what I am looking for. Provide multiple ways of searching for the art
+        via the gallery app. Can put genre, artist, and multiple genres. Provide
+        a search bar. Personas Tiffany Young Problem statement: Tiffany Young is
+        an art dealer who needs to be able to view art virtually because she
+        doesn’t have time to travel. IMAGE OF PERSONA / with stuff on it User
+        Journey Map Mapping Tiffany’s journey shows several problems that could
+        arise with the app. Definitely simplicity/ conciseness in the menu style
+        and high quality photos are a must. Persona: Tiffany Young Goal: Find
+        new art pieces for my gallery. IMAGE OF USER JOURNEY MAP
+      </div>
+      <div>
+        Starting The Design - Ideate / Prototype / Test Paper Wireframes Goals
+        for paper wireframes: The paper wireframes should have plenty of space
+        for the images since it is an image centered app. Digital Wireframes SEE
+        SLIDESHOW IMAGE INSERT GRAPHIC WITH LABELS Low-Fidelity Prototypes Link
+        to low fi prototypes
+        https://www.figma.com/proto/BkvbbGLoYEyj2PUKLNQsWp/EdinburghGalleryApp?node-id=1%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=8%3A415
+        Usability Studies I conducted two rounds of usability studies. Findings
+        from the first study helped guide the designs from wireframes to
+        mockups. The second study used a high fidelity prototype and revealed
+        what aspects of the mockups needed refining. Round One Findings: 1.
+        Users want to easily find what art They are looking for. 2. Users want
+        to be able to view art in Great detail. Round Two Findings: 1. Users
+        want to be able to be directed somewhere after filling out contact form.
+        2. Users want to get a confirmation email.
+      </div>
+      <div>
+        Refining the Design: High-Fidelity Prototype
         https://www.figma.com/proto/tOggtjWuy6ImMI2hmPZLEx/High-Fidelity-Prototypes?node-id=2%3A455&starting-point-node-id=2%3A455
-        User Testing
-            Accessibility Considerations
-                INSERT BEFORE AND AFTER IMAGE OF WHAT WAS FIXED
-
-
-
-
-
-
-
+        User Testing Accessibility Considerations INSERT BEFORE AND AFTER IMAGE
+        OF WHAT WAS FIXED
+      </div>
     </div>
-   
-
-
-
-
-</div>;
+  </div>
+);
 
 export default ArtGallery;
