f9da2457cc1a2a7e4ff327bc56eaeeb553a02fa0
Katie Stout <katie.stout@outlook.com>
2022-09-01 15:29:31 -0400

    front layout

--- portfolio/src/routes/projects/components/mainPage.js
+++ portfolio/src/routes/projects/components/mainPage.js
@@ -8,24 +8,76 @@ const MainPage = () => (
     <div className="project-container">
       <Link to="/crewCuts">
         <div className="project project-1">
-          {" "}
-          crewcuts
-          <img src={ccGraphic} alt="ccGraphic" className="ccGraphic" />
+          <div className = "div1">
+          <div>
+            <span className = "project-title">
+            Crewcuts </span>
+            <br/>
+            <span className = "project-content">skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj</span>
+            </div>
+           
+            
+          </div>
+          <div>
+          <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
+          </div>
         </div>
       </Link>
+
       <Link to="/mpp">
-        {" "}
-        <div className="project project-2"> mpp</div>
+        <div className="project project-2">
+        <div className = "div1">
+        <div>
+            <span className = "project-title">
+            MPP </span>
+            <br/>
+            <span className = "project-content">skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj</span>
+            </div>
+           
+            
+          </div>
+          <div>
+          <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
+          </div>
+        </div>
       </Link>
 
       <Link to="/artGallery">
-        {" "}
-        <div className="project project-3">artGallery</div>
+        <div className="project project-3">
+        <div className = "div1">
+        <div>
+            <span className = "project-title">
+            Art Gallery App </span>
+            <br/>
+            <span className = "project-content">skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj</span>
+            </div>
+            
+            
+          </div>
+          <div>
+          <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
+          </div>
+        </div>
       </Link>
 
       <Link to="/portfolio">
-        <div className="project project-4"> portfolio</div>
+        <div className="project project-4">
+        <div className = "div1">
+            <div>
+            <span className = "project-title">Portfolio</span>
+            <br/>
+            <span className = "project-content">skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj</span>
+            </div>
+           
+            
+          </div>
+          <div>
+          <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
+          </div>
+        </div>
       </Link>
+
+
     </div>
   </div>
 );
