c4d0d28c4118879867a0ba43e7eb0a992b6f09ac
Katie Stout <katie.stout@outlook.com>
2022-09-01 17:45:54 -0400

    prettier

--- portfolio/src/routes/projects/components/mainPage.js
+++ portfolio/src/routes/projects/components/mainPage.js
@@ -2,82 +2,104 @@ import React from "react";
 import styles from "./../styles/mainPage.scss";
 import { Link } from "react-router-dom";
 import ccGraphic from "./../../../svg/crewCuts/crewcutssq.png";
+import galleryGraphic from "./../../../svg/gallery/galleryFront.png";
+import mppGraphic from "./../../../svg/mpp/mppFront.png";
 
 const MainPage = () => (
   <div className={styles}>
     <div className="project-container">
       <Link to="/crewCuts">
         <div className="project project-1">
-          <div className = "div1">
-          <div>
-            <span className = "project-title">
-            Crewcuts </span>
-            <br/>
-            <span className = "project-content">skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj</span>
+          <div className="div1">
+            <div className="div2">
+              <span className="project-title">CrewCuts </span>
+              <br />
+              <span className="project-content">
+                <ul>
+                  <li>Mobile App</li>
+                  <li>Adobe XD</li>
+
+                  <li>Affinity Designer</li>
+                  <li>Real-World project</li>
+                </ul>
+              </span>
             </div>
-           
-            
           </div>
-          <div>
-          <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
+
+          <div className="div3">
+            <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
           </div>
         </div>
       </Link>
 
       <Link to="/mpp">
         <div className="project project-2">
-        <div className = "div1">
-        <div>
-            <span className = "project-title">
-            MPP </span>
-            <br/>
-            <span className = "project-content">skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj</span>
+          <div className="div1">
+            <div className="div2">
+              <span className="project-title">MPP </span>
+              <br />
+              <span className="project-content">
+                <ul>
+                  <li>Website </li>
+                  <li>Adobe XD </li>
+                  <li>Affinity Designer</li>
+                  <li> Previous Work Experience</li>
+                </ul>
+              </span>
             </div>
-           
-            
           </div>
-          <div>
-          <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
+          <div className="div3">
+            <img src={mppGraphic} alt="mppGraphic" className="front-graphic" />
           </div>
         </div>
       </Link>
 
       <Link to="/artGallery">
         <div className="project project-3">
-        <div className = "div1">
-        <div>
-            <span className = "project-title">
-            Art Gallery App </span>
-            <br/>
-            <span className = "project-content">skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj</span>
+          <div className="div1">
+            <div className="div2">
+              <span className="project-title">Art Gallery</span>
+              <br />
+              <span className="project-content">
+                <ul>
+                  <li>Mobile App </li>
+                  <li>Figma</li>
+                  <li>Google UI UX Certificate Course</li>
+                </ul>
+              </span>
             </div>
-            
-            
           </div>
-          <div>
-          <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
+          <div className="div3">
+            <img
+              src={galleryGraphic}
+              alt="galleryGraphic"
+              className="front-graphic"
+            />
           </div>
         </div>
       </Link>
 
       <Link to="/portfolio">
         <div className="project project-4">
-        <div className = "div1">
-            <div>
-            <span className = "project-title">Portfolio</span>
-            <br/>
-            <span className = "project-content">skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj</span>
+          <div className="div1">
+            <div className="div2">
+              <span className="project-title">Portfolio</span>
+              <br />
+              <span className="project-content">
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                skhkahsdfklhaksfdh jkshfdkashdflk ksjdhfkaj
+              </span>
             </div>
-           
-            
           </div>
-          <div>
-          <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
+          <div className="div3">
+            <img src={ccGraphic} alt="ccGraphic" className="front-graphic" />
           </div>
         </div>
       </Link>
-
-
     </div>
   </div>
 );
