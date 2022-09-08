cf23da603a6c697ab207b82a8ac38581a8c85627
Katie Stout <katie.stout@outlook.com>
2022-09-08 11:47:11 -0400

    hamburger menu fix

--- portfolio/src/components/header/Header.js
+++ portfolio/src/components/header/Header.js
@@ -1,12 +1,10 @@
 import styles from "./header.scss";
-import headerGraphic from "./../../svg/headerGraphic.png";
+import headerGraphic from "./../../svg/headerGraphic2.png";
 
 const Header = () => (
   <div className={styles}>
     <img src={headerGraphic} alt="headerGraphic" className="headerGraphic" />
     <header>
-      {/* <h1>Katie Stout</h1>
-      <h2>UI/UX Designer </h2> */}
     </header>
   </div>
 );
