5fc43cb279a0da83107fcaa0568ee5af671aec66
Katie Stout <katie.stout@outlook.com>
2022-09-28 13:23:44 -0400

    about me page

--- portfolio/src/routes/about/components/aboutPage.js
+++ portfolio/src/routes/about/components/aboutPage.js
@@ -1,23 +1,45 @@
 //import React from 'react';
 import styles from "./../styles/aboutPage.scss";
 import selfie from "./../../../svg/selfie.png";
+import icon from "./../../../svg/katieicon.png";
 
 const AboutPage = () => (
   <div className={styles}>
-    <img src={selfie} alt="graphic" className="selfie" />
-    <p className="bio">
-      I'm a UI/UX Designer and Front End Web Developer, with a background in
-      Painting and Drawing. I am also a wedding and portrait Photographer! I
-      absolutely love to create wireframes, design icons, and fiddle with CSS
-      animations. While I have experience as a Full Stack Developer, what I
-      really enjoy is the visual aspect of User Experience Design, as it has
-      such an impact overall on what we create.
-    </p>
+    <div className = "aboutPage">
 
+
+    <div className = "parent">
+      <div className="div1"><img src={selfie} alt="graphic" className="selfie" /></div>
+      <div className="div2">    <p className="bio">
+        <div className="title">Hi There!</div>
+        <p>
+      My name is Katie Stout, I'm a UI UX Designer and Front End Web Developer, with a background in
+      painting and drawing. 
+      
+      
+      I absolutely love to create wireframes, to design icons and graphics, and to fiddle with CSS. 
+       
+      I have several years experience as a Full Stack Developer. What I
+      really enjoy is the visual aspect of User Experience Design, as it has
+      such an impact overall on what we create. 
+
+      It is rewarding to come up with ideas for websites, and then be able to help create your own vision.
+      </p>
     <p>
-      A link to my previous portfolio (2019):
-      https://katherinestout.github.io/KatStout_ReactPortfolio/
-    </p>
+      Among my many hobbies I enjoy photography  (portrait and food photography), and I have recently begun to have an obsession with film cameras. 
+      On a day off you will most likely find me on an outdoor adventure or hanging out with friends, typically with a camera within reach.
+      </p>
+    </p></div>
+      <div className="div3">
+        <div className = "iconDiv">
+        <img src={icon} alt="icon" className="icon" />
+        </div>
+        
+       </div>
+
+    </div>
+    
+    </div>
   </div>
 );
 export default AboutPage;
--- portfolio/src/routes/projects/components/artGallery/artGallery.js
+++ portfolio/src/routes/projects/components/artGallery/artGallery.js
@@ -37,9 +37,8 @@ const ArtGallery = () => (
               obtaining art pieces such as art collectors ) with other artists
               via the app. This App was one of the projects completed from
               beginning to end while I obtained the Certificate in UX design
-              from Coursera. We utilized the five stages of the design thinking
-              process: Empathize Define Ideate Prototype Test Product Duration
-              October 2021 - December 2021
+              from Google. We utilized the five stages of the design thinking
+              process: Empathize, Define, Ideate, Prototype, and Test. 
             </div>
           </div>
         </div>
@@ -65,61 +64,61 @@ const ArtGallery = () => (
             <div className="subHeader">Solution </div>
 
             <div className="content">
-              The goal was to create an app that allowed for users to easily
-              find art they wanted to obtain and to be able to contact the
-              artist, to spark the conversation that could eventually lead to
-              more opportunities for the artist such as the user purchasing
-              their art or their art being showed in other galleries as well.
+The goal was to create an app that allowed for users to easily
+find art they wanted to obtain and to be able to contact the
+artist, to spark the conversation that could eventually lead to
+more opportunities for the artist such as the user purchasing
+their art or their art being showed in other galleries as well.
             </div>
           </div>
         </div>
 
         <div className="wrapper-4">
           <div className="subHeader div1">
-            Understanding the User: <i>Empathize and Define</i>
-          </div>
+Understanding the User: <i>Empathize and Define</i>
+</div>
 
-          <div className="header div2">User Research Summary </div>
+<div className="header div2">User Research Summary </div>
 
-          <div className=" div3 content">
-            I found that users wanted a variety of art to select from. Users
-            also wanted to be able to contact artists directly and from the
-            specific gallery websites. Often galleries don’t list contact
-            information and make users directly contact the gallery instead.
-            Users also wanted to be able to see art and maybe not necessarily
-            purchase it, and because of the pandemic galleries should adapt to
-            an online audience.
-          </div>
+<div className=" div3 content">
+I found that users wanted a variety of art to select from. Users
+also wanted to be able to contact artists directly and from the
+specific gallery websites. Often galleries don’t list contact
+information and make users directly contact the gallery instead.
+Users also wanted to be able to see art and maybe not necessarily
+purchase it, and because of the pandemic galleries should adapt to
+an online audience.
+</div>
         </div>
 
-        <div className="wrapper-5">
-          <div className="div1 subHeader">User Pain Points</div>
+<div className="wrapper-5">
+<div className="div1 subHeader">User Pain Points</div>
 
-          <div className="div2 content">
-            1. Galleries don't have a way for me to contact artists.
-          </div>
-          <div className="div3 boldContent">
-            Provide multiple ways to contact artists, such as a contact form,
-            the artist’s contact information, and the artist’s individual
-            website.
-          </div>
+<div className="div2 content">
+1. Galleries don't have a way for me to contact artists.
+</div>
+<div className="div3 boldContent">
+Provide multiple ways to contact artists, such as a contact form,
+the artist’s contact information, and the artist’s individual
+website.
+</div>
 
-          <div className="div4 content">
-            {" "}
-            2. Galleries often have limited selection of art, I cannot find what
-            I am looking for.{" "}
-          </div>
+<div className="div4 content">
+{" "}
+2. Galleries often have limited selection of art, I cannot find what
+I am looking for.{" "}
+</div>
 
-          <div className="div5 boldContent">
-            Provide multiple ways of searching for the art via the gallery app.
-            Can put genre, artist, and multiple genres. Provide a search bar.
-          </div>
+<div className="div5 boldContent">
+Provide multiple ways of searching for the art via the gallery app.
+Can put genre, artist, and multiple genres. Provide a search bar.
+</div>
 
-          <div className="div6 content">
-            3. Because of the pandemic, I cannot get to galleries in person.{" "}
-          </div>
+<div className="div6 content">
+3. Because of the pandemic, I cannot get to galleries in person.{" "}
+</div>
 
-          <div className="div7 boldContent">Provide high quality images.</div>
+<div className="div7 boldContent">Provide high quality images.</div>
         </div>
 
         <div className="wrapper-6">
@@ -153,11 +152,11 @@ const ArtGallery = () => (
               />
             </div>
 
-            <div className="content">
-              Mapping Tiffany’s journey shows several problems that could arise
-              with the app. Definitely simplicity/ conciseness in the menu style
-              and high quality photos are a must.
-            </div>
+<div className="content">
+Mapping Tiffany’s journey shows several problems that could arise
+with the app. Definitely simplicity/ conciseness in the menu style
+and high quality photos are a must.
+</div>
           </div>
         </div>
 
@@ -215,28 +214,28 @@ const ArtGallery = () => (
           </div>
         </div>
 
-        <div className="wrapper-9">
-          <div className="div1 header"> Usability Studies</div>
-          <div className="div2 content">
-            I conducted two rounds of usability studies. Findings from the first
-            study helped guide the designs from wireframes to mockups. The
-            second study used a high fidelity prototype and revealed what
-            aspects of the mockups needed refining.
-          </div>
-          <div className="div3 content">
-            <div className="bold">Round One Findings:</div>
-            1.Users want to easily find what art They are looking for.
-            <br />
-            2. Users want to be able to view art in Great detail.
-          </div>
-          <div className="div4 content">
-            <div className="bold">Round Two Findings:</div>
-            1. Users want to be able to be directed somewhere after filling out
-            contact form.
-            <br />
-            2. Users want to get a confirmation email.
-          </div>
-        </div>
+<div className="wrapper-9">
+<div className="div1 header"> Usability Studies</div>
+<div className="div2 content">
+I conducted two rounds of usability studies. Findings from the first
+study helped guide the designs from wireframes to mockups. The
+second study used a high fidelity prototype and revealed what
+aspects of the mockups needed refining.
+</div>
+<div className="div3 content">
+<div className="bold">Round One Findings:</div>
+1.Users want to easily find what art They are looking for.
+<br />
+2. Users want to be able to view art in Great detail.
+</div>
+<div className="div4 content">
+<div className="bold">Round Two Findings:</div>
+1. Users want to be able to be directed somewhere after filling out
+contact form.
+<br />
+2. Users want to get a confirmation email.
+</div>
+</div>
 
         <div className="wrapper-10">
           <div className="div1 subHeader">Refining the Design</div>
