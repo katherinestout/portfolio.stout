7b282178c26c64f041e6433ca2bf5fdcbb8be842
Katie Stout <katie.stout@outlook.com>
2022-10-09 14:02:42 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import styles from "./global/sass/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Router>
+    <div className={styles}>
+      <Header />
+      <Nav />
+      <Footer />
+    </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
